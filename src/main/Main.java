c6209f1197895c0c5ca9fcf46a7a3917c969930f
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-10 06:33:12 +0000

    build the main splash ui

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,21 @@
 package main;
 
-public class Main {
-    
+import javafx.stage.Stage;
+import javafc.scene.Scene;
+
+public class Main extends Application {
+
+    @override
+    public void start(Stage stage) {
+
+        BorderPane root = new BorderPane();
+
+        Scene scene = new Scene(root, 945, 700);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
