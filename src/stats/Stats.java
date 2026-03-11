6cd742aa8ce73c92a66c9678f4b1814332e53445
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-11 18:35:48 +0000

    new funcs : getReport()

--- src/stats/Stats.java
+++ src/stats/Stats.java
@@ -1,7 +1,7 @@
 package stats;
 
 public class Stats {
-    double sum = 0.0 ,mean = 0.0, max = 0.0, min = 0.0, count = 0.0;
+    double sum = 0.0, mean = 0.0, max = 0.0, min = 0.0, count = 0.0;
 
     public Stats(Object[] list) {
         for (Object item : list) {
@@ -14,5 +14,27 @@ public class Stats {
             // sum
             sum += (double) item;
         }
+    }
+
+    public String getReport() {
+        return "" +
+                " ============ REPORT ============" +
+                "|   Max  : " + max +
+                "|   Min  : " + min +
+                "|   Mean : " + mean +
+                "|   Count: " + count +
+                "|   Sum  : " + sum +
+                "|__________________________________";
+    }
+
+    public String getReport(String forLabel) {
+        return "" +
+                " ============ REPORT for '" + forLabel + "' ============" +
+                "|   Max  : " + max +
+                "|   Min  : " + min +
+                "|   Mean : " + mean +
+                "|   Count: " + count +
+                "|   Sum  : " + sum +
+                "|__________________________________";
     }
 }
