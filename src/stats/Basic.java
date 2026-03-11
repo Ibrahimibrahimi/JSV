567e9d1b96b0af1cc1daba6469cd1767ae8788a4
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-11 18:24:41 +0000

    new funcs : sum , count

--- src/stats/Basic.java
+++ src/stats/Basic.java
@@ -1,6 +1,15 @@
 package stats;
 
 public class Basic {
+
+    public static double sum(Object[] list) {
+        double sum = 0.0;
+        for (Object item : list) {
+            sum += (double) item;
+        }
+        return sum;
+    }
+
     public static double mean(Object[] list) {
         double result = 0.0;
         int count = 0;
@@ -14,9 +23,24 @@ public class Basic {
     public static double max(Object[] list) {
         double max = 0.0;
         for (Object item : list) {
-            max = (double) item > max ? (double) item : max;
+            max = ((double) item) > max ? (double) item : max;
         }
         return max;
     }
 
+    public static double min(Object[] list) {
+        double min = 0.0;
+        for (Object item : list) {
+            min = ((double) item) < min ? (double) item : min;
+        }
+        return min;
+    }
+
+    public static double count(Object[] list) {
+        int count = 0;
+        for (Object item : list) {
+            count++;
+        }
+        return count;
+    }
 }
